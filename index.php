9af2db4af5817a76d79cc3c6b54b94ab435865b1
David <sdsteve@f45c89ba4fe5.ant.amazon.com>
2018-03-31 21:51:02 -0700

    trying to fix the image

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
         <div align="center">
                 <h1>Hello Immersion Day people</h1>
                 <h2>Congratulations! You have successfully installed an application to OpsWorks!</h2>
-                <div align="center"><img src="AWS_logo.png"></div>
+                <div align="center"><img src="/AWS_logo.png"></div>
                 <p>The application is now running on the host &ldquo;<?php echo gethostname(); ?>&rdquo; in a new stack and layer in OpsWorks.</p>
                 <p>This host is running PHP version <?php echo phpversion(); ?>.</p>
         </div>
